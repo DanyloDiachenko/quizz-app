cfc6ced87d5d4bdd25aad09ab5d724c1b20b89e6
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-02-12 23:19:00 +0200

    achiev fix

--- data/getAchievements.ts
+++ data/getAchievements.ts
@@ -7,13 +7,25 @@ export const getAchievements = (
     ownQuests: Quest[],
 ) => {
     const possibleAchievements = [
-        { name: "Топ рейтинг", condition: profile.rating >= 5 },
-        { name: "Квестер", condition: completedQuests.length >= 10 },
+        { name: "Топ рейтинг", condition: profile.rating >= 4 },
+        { name: "Квестер 1", condition: completedQuests.length >= 1 },
+        { name: "Квестер 2", condition: completedQuests.length >= 3 },
+        { name: "Квестер 3", condition: completedQuests.length >= 5 },
         {
-            name: "Майстер квестів",
+            name: "Майстер квестів 1",
+            condition: ownQuests.length >= 1,
+        },
+        {
+            name: "Майстер квестів 2",
+            condition: ownQuests.length >= 3,
+        },
+        {
+            name: "Майстер квестів 3",
             condition: ownQuests.length >= 5,
         },
-        { name: "Дослідник", condition: completedQuests.length >= 5 },
+        { name: "Дослідник 1", condition: completedQuests.length >= 1 },
+        { name: "Дослідник 2", condition: completedQuests.length >= 3 },
+        { name: "Дослідник 3", condition: completedQuests.length >= 5 },
     ];
 
     const unlockedAchievements = possibleAchievements
